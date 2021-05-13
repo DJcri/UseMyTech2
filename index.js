932c9b739bd2eb3665bbb2947c577bdc2480eac5
DJcri <djcri123@gmail.com>
2021-05-13 15:16:43 -0700

    turned into monolith app

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = require("./api/server");
 
 const port = process.env.PORT || 5000;
 
-server.use(express.static(path.join(__dirname, "client/dist")));
+server.use(express.static(path.join(__dirname, "client/build")));
 
 server.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/dist", "index.html"));
