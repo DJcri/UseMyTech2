9b5f5d48d8f847ebb9fcc9b07f111df7c3e1a70f
DJcri <djcri123@gmail.com>
2021-05-13 16:11:25 -0700

    test

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 5000;
 
 server.use(express.static(path.join(__dirname, "client/build")));
 
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 server.listen(port, () => {
   console.log("listening on " + port);
 });
