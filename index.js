8f5c4503e6d419e969dbe3c736b00657ced7afd4
DJcri <djcri123@gmail.com>
2021-05-13 15:37:24 -0700

    added postbuild script

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const port = process.env.PORT || 5000;
 
 server.use(express.static(path.join(__dirname, "client/build")));
 
-server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
-});
-
 server.listen(port, () => {
   console.log("listening on " + port);
 });
